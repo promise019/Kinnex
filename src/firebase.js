bc55a6027fa398a63a691a7f342cda974cbd1a64
promiseedet <edetpromise019@gmail.com>
2025-07-02 17:16:37 +0100

    firebase config added

--- src/firebase.js
+++ src/firebase.js
@@ -3,16 +3,21 @@ import { connectAuthEmulator, getAuth } from "firebase/auth";
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 
 const firebaseConfig = {
- 
+  apiKey: "AIzaSyBO0toGfMzc6sTmPJwS6N19z2cEoam2L2w",
+  authDomain: "kinnex-b7001.firebaseapp.com",
+  projectId: "kinnex-b7001",
+  storageBucket: "kinnex-b7001.firebasestorage.app",
+  messagingSenderId: "209766831689",
+  appId: "1:209766831689:web:b55e44f79d8da5b0c09198"
 };
 
   const app = initializeApp(firebaseConfig)
   const auth = getAuth(app)
   const db = getFirestore(app)
 
-if (window.location.hostname === "localhost") {
-    connectAuthEmulator(auth, 'http://localhost:9099')
-    connectFirestoreEmulator(db, "localhost", 8080)
-}
+// if (window.location.hostname === "localhost") {
+//     connectAuthEmulator(auth, 'http://localhost:9099')
+//     connectFirestoreEmulator(db, "localhost", 8080)
+// }
 
   export {auth, db}
