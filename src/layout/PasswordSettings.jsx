3c44a29b0a38f371807befe01fa3cbf659b23ae9
promiseedet <edetpromise019@gmail.com>
2025-08-15 13:49:03 +0100

    Reset password done

--- src/layout/PasswordSettings.jsx
+++ src/layout/PasswordSettings.jsx
@@ -1,14 +1,24 @@
-import { Backend } from "firebase/ai";
 import passkey from "../assets/icon/passkey.svg";
 import Button from "../component/Button";
-import info from '../assets/icon/Frame (12) (1).svg'
+import info from "../assets/icon/Frame (12) (1).svg";
+import { toast, ToastContainer } from "react-toastify";
+import { userDataContext } from "../context/UserDataContext";
+import { useContext } from "react";
+import { sendPasswordResetEmail } from "firebase/auth";
+import { auth } from "../firebase";
 
 export default function PasswordSettings() {
-  const updatePassword = async (e)=>{
-    e.preventDefault()
-  }
+  const { referralData } = useContext(userDataContext);
+  const updatePassword = (e) => {
+    e.preventDefault();
+    sendPasswordResetEmail(auth, referralData.email)
+      .then((s) => toast.info("Reset Link has been sent to your Email Address"))
+      .catch((e) => toast.error(`Error sending reset link: ${e}`));
+  };
+  
   return (
     <section className="rounded-tr-xl w-full bg-white shadow p-2 space-y-4 space-x-2">
+      <ToastContainer />
       <h1 className="font-bold">Passsword Settings</h1>
       <img src={passkey} className="w-6 inline-block" />
       <span>Change Password</span>
@@ -20,7 +30,10 @@ export default function PasswordSettings() {
       </Button>
       <div className="bg-blue-300 text-blue-800 flex p-2 space-x-2 rounded-lg">
         <img src={info} className="w-7" />
-        <p>A link will be sent to your email address, click on it to Reset your password</p>
+        <p>
+          A link will be sent to your email address, click on it to Reset your
+          password
+        </p>
       </div>
     </section>
   );
